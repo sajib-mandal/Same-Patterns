a03b89aae681ce33e0d0792c11eccf329c668836
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-11-07 17:46:48 +0530

    Create isIsomorphic.py

---
+++ Two HashMap/isIsomorphic.py
@@ -0,0 +1,31 @@
+#   205. Isomorphic Strings
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapST, mapTS = {}, {}
+        for c1, c2 in zip(s, t):
+            if ((c1 in mapST and mapST[c1] != c2) or 
+                (c2 in mapTS and mapTS[c2] != c1)):
+                return False
+            mapST[c1] = c2
+            mapTS[c2] = c1
+        return True
+        
+        
+        if len(s) != len(t):
+            return False
+        mapST = {}
+        mapTS = {}
+        
+        for i in range(len(s)):    # Both are same size
+            c1, c2 = s[i], t[i]
+            if ((c1 in mapST and mapST[c1] != c2) or
+                (c2 in mapTS and mapTS[c2] != c1)):
+                return False
+            mapST[c1] = c2
+            mapTS[c2] = c1
+        return True
+        
