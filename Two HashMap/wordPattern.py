383a5f659583cb49bf0539761956241c3db862ee
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-11-07 17:57:31 +0530

    Update wordPattern.py

--- Two HashMap/wordPattern.py
+++ Two HashMap/wordPattern.py
@@ -1,19 +1,29 @@
-#  290. Word Pattern
-
+# 205. Isomorphic Strings
 
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.split(" ")
-        if len(words) != len(pattern):
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
             return False
-        
-        charToWord = {}
-        wordToChar = {}
-        
-        for c, w in zip(pattern, words):
-            if ((c in charToWord and charToWord[c] != w) or 
-                (w in wordToChar and wordToChar[w] != c)):
+        mapST, mapTS = {}, {}
+        for c1, c2 in zip(s, t):     # It's easy
+            if ((c1 in mapST and mapST[c1] != c2) or 
+                (c2 in mapTS and mapTS[c2] != c1)):
                 return False
-            charToWord[c] = w
-            wordToChar[w] = c
+            mapST[c1] = c2
+            mapTS[c2] = c1
+        return True
+        
+        
+        if len(s) != len(t):
+            return False
+        mapST = {}
+        mapTS = {}
+        
+        for i in range(len(s)):    # Both are same size
+            c1, c2 = s[i], t[i]
+            if ((c1 in mapST and mapST[c1] != c2) or
+                (c2 in mapTS and mapTS[c2] != c1)):
+                return False
+            mapST[c1] = c2
+            mapTS[c2] = c1
         return True
